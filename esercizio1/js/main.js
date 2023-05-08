0e09d7020927e98e2cde8acebc8583e9c9a4b3e4
Matteo Sarrecchia <morrea@hotmail.it>
2023-05-08 15:28:38 +0200

    struttra e funzioni

---
+++ esercizio1/js/main.js
@@ -0,0 +1,39 @@
+// Chiedere all’utente di inserire una parola
+const parolaUser = prompt("Inserisci una parola");
+console.log(parolaUser);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// FUNZIONE INVERTI PAROLA
+function invertiParola (string){
+
+    let revertParolaUSer = string.split("");
+    // console.log(revertParolaUSer)
+
+    let arrayParola =revertParolaUSer.reverse();
+    // console.log(arrayParola)
+
+    let newParolaUSer = arrayParola.join("");
+    // console.log(newParolaUSer)
+
+    return newParolaUSer;
+
+};
+
+let newRevertParolaUSer = invertiParola(parolaUser);
+console.log(newRevertParolaUSer)
+
+
+// FUNZIONE VERIFICA PAROLA PALINDORMA
+function verificaParolaPalindroma (string){
+
+    let parolaPalindroma
+
+    if (parolaUser === newRevertParolaUSer){
+
+        return parolaUser + " é una parola palindroma"
+    } else return  parolaUser + " NON é una parola palindroma"
+};
+
+
+console.log(verificaParolaPalindroma(parolaUser));
